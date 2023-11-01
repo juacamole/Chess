ad0992ddcbc771094f3213d5fb167e840d294474
LUEJ9 <juan.luethy@coop.ch>
2023-11-01 11:32:47 +0100

    UPDATE: garnix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -13,12 +15,21 @@ public class Main {
             b = b + a[i];
         }
         System.out.println(b);
-        System.out.print("Grösste zahl " + a[a.length - 1]);
+        System.out.println("Grösste zahl " + a[a.length - 1]);
 
-        int[] c = new int[20];
+        int[] c = new int[10];
         for (int i = 10; i < 20; i++) {
-            a[i] = i + 1;
-            System.out.println(c[i]);
+            c[i -10] = i + 1;
+            System.out.println(c[i -10]);
+        }
+
+        int[] d = new int[10];
+        for (int i = 0; i < 10; i++) {
+            d[i] = a[i] + c[i];
+
+        }
+        for (int z = 0; z < d.length; z++){
+            System.out.println(d[z]);
         }
     }
 }
